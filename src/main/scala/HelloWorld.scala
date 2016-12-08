258c74f2b7dead8940e20528ba354d3feccc3789
최경민 <chgm1006@gmail.com>
2016-12-08 18:33:11 +0900

    test

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,12 +1,5 @@
-object HelloWorld extends App {
-  val employees = List("neal", "s", "stu", "j", "rich", "bob", "aiden", "j", "ethan", "liam", "mason", "noah", "lucas", "jacob", "jayden", "jack")
-
-  val result = employees
-    .filter(_.length() > 1)
-    .map(_.capitalize)
-    .reduce(_ + ", " + _)
-
-  println(result)
-
-
+object HelloWorld{
+  def main(args:Array[String]): Unit = {
+    print("Hello, world!!")
+  }
 }
